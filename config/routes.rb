596d2831bade2db84fcc20c78e60aaa815117835
rakuraku.musen <musen.rakuraku@gmail.com>
2018-09-12 09:48:37 +0000

    have ranking

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
   
   get "rankings/want" => "rankings#want"
+  get "rankings/have" => "rankings#have"
   
   resources :users, only: [:show, :new, :create]
   resources :items, only: [:show,:new]
