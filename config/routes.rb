c17f4863a77caf2b5b58d57e8340a5ba2dd87262
rakuraku.musen <musen.rakuraku@gmail.com>
2018-09-12 07:10:57 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   delete "logout" => "sessions#destroy"
   
   get "signup" => "users#new"
+  
+  get "rankings/want" => "rankings#want"
+  
   resources :users, only: [:show, :new, :create]
   resources :items, only: [:show,:new]
   resources :ownerships, only: [:create, :destroy]
